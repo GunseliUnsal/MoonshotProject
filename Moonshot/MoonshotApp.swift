3f61f74e6d5b4267b49f762c911c34422ccf9450
Günseli Ünsal <93467749+GunseliUnsal@users.noreply.github.com>
2024-07-09 15:37:05 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Günseli Ünsal on 9.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
